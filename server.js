a16d99f819eebe3a2d28dfb1e12d90bcfe6413bb
Kalpesh Wadile <thisismekalpesh777@gmail.com>
2026-03-23 17:26:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-XSS-Protection', '1; mode=block');
   res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+  res.setHeader('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
   next();
 });
 
